2c17624f64b63db8743aeb9435aaa012f0e268ae
RajibulAlam <rajibulAlam@gmail.com>
2019-02-01 01:13:45 -0600

    Add files via upload

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,12 +5,29 @@ var budgetController = ( function () {
 
 var UIController = (function () {
 
+        var DOMstrings = {
+            inputType: '.add__type',
+            inputDescription: '.add__description',
+            inputValue: '.add__value'
+        };
+
+        return {
+            getInput: function() {
+                return {
+                    type: document.querySelector(DOMstrings.inputType).value,
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value
+                };
+            }  
+        };
 })();
 
 var appContoller = (function(bugetCtrl, UICtrl){
 
     var ctrlAddItem = function() {
-        console.log("pressed");
+        
+        var input =UICtrl.getInput();
+        console.log(input);
     }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem());
