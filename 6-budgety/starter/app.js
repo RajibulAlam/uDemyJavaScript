ce74d40da73e06f2af4f78411f0ee57576e30890
RajibulAlam <rajibulAlam@gmail.com>
2019-02-08 17:00:48 -0600

    Add files via upload

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,5 +1,53 @@
 var budgetController = ( function () {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp:0,
+            inc:0
+        }
+    };
+
+    return {
+        addItem: function (type, itemDesc, itemValue) {
+            var newItem;
+            var ID = 0;
+
+            if (data.allItems[type].length>0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1; 
+            }
+            //Math.round(Math.random() * 100);
+
+            if( type === 'exp') {
+                newItem = new Expense(ID, itemDesc, itemValue);
+            }  else if (type === 'inc') {
+                newItem = new Income(ID, itemDesc, itemValue);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem; 
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    }
+
 })();
 
 
@@ -44,6 +92,8 @@ var appContoller = (function(bugetCtrl, UICtrl){
 
     var ctrlAddItem = function() {
         var input =UICtrl.getInput();
+
+        budgetController.addItem(input.type,input.description,input.value);
         console.log(input);
     }
 
